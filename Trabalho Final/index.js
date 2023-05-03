6491c3da17709d8e7a3ba3193f0134eda377e36f
RafaelCarvalho90 <carvalho_rafael@live.com>
2023-05-03 18:49:32 -0300

    Upload dados

---
+++ Trabalho Final/index.js
@@ -0,0 +1,14 @@
+console.log("Inicio do JavaScript")
+
+let dados;
+
+function simulaGet (){
+    setTimeout(() => {
+        console.log("Dentro do SetTimeOut")
+    }, 3000);
+
+}
+
+simulaGet();
+
+console.log("Fim do JavaScript");
